9d71f49450acdfb84d6ae06fb32c82f4c7e0034a
ytomino <demoonlit@panathenaia.halfmoon.jp>
2018-05-05 08:57:13 +0900

    Fix for OpenSSL 1.1.0g

--- source/import.h
+++ source/import.h
@@ -1,5 +1,9 @@
 #if defined(__gnu_linux__)
 #include <sys/types.h> /* ssize_t */
 #endif
+#define OPENSSL_NO_DEPRECATED
+#define OPENSSL_NO_FILENAMES
 #include <openssl/md5.h>
 #include <openssl/sha.h>
+#undef OPENSSL_NO_DEPRECATED
+#undef OPENSSL_NO_FILENAMES
